7eb4c7104447f4cb3713924f09d922e447eeba4a
Mandea Daniel Mihai <daniel.mandea@ro.ibm.com>
2017-04-08 09:09:56 +0300

    task 23 Done adding seve with completion method as public / some formating updates.

--- CoreDataStackHelper/Classes/NSManagedObjectContext.swift
+++ CoreDataStackHelper/Classes/NSManagedObjectContext.swift
@@ -9,19 +9,19 @@
 import Foundation
 import CoreData
 
-extension NSManagedObjectContext {
-    /**
-     Use this function in order to save some context
-     */
-    func save(completion: @escaping CompletionBlock) {
-        if self.hasChanges {
-            do {
-                try self.save()
-                completion(true, nil)
-            } catch let error {
-                completion(false, error)
-            }
-        }
+public extension NSManagedObjectContext {
+  /**
+  Use this function in order to save some context
+  */
+  public func save(completion: @escaping CompletionBlock) {
+    if self.hasChanges {
+      do {
+        try self.save()
+        completion(true, nil)
+      } catch let error {
+        completion(false, error)
+      }
     }
-    
+  }
+  
 }
