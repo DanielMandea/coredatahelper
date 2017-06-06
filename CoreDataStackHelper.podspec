b2f0fcfe84b1241565cf58f3180b2639549d0aaa
Mandea Daniel Mihai <daniel.mandea@ro.ibm.com>
2017-06-06 14:44:25 +0300

    task 56 Added some updates to core data manager / Release 0.1.3

--- CoreDataStackHelper.podspec
+++ CoreDataStackHelper.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'CoreDataStackHelper'
-  s.version          = '0.1.2'
+  s.version          = '0.1.3'
   s.summary          = 'Lightweight CoreData helper for creting / saving anf fetching NSManageObjects'
 
 # This description is used to generate tags and improve search results.
--- CoreDataStackHelper/Classes/NSManagedObject.swift
+++ CoreDataStackHelper/Classes/NSManagedObject.swift
@@ -16,112 +16,112 @@ public typealias CompletionBlock = (_ success:Bool, _ error: Error?) -> Void
 
 /// Us ethis protocol when using Dictionary / JSON
 public protocol UpdateManangedObject: class {
-  /// Use this method in order to update a NSManagedObject
-  func update(with data:Any)
+    /// Use this method in order to update a NSManagedObject
+    func update(with data:Any)
 }
 
 // MARK: - AddManagedObject
 
 public protocol AddManagedObject: class {
-  /**
-   This method is called by all managedObjects in order to add / update
-   - parameter data:      The data received for update
-   - parameter context:   The context that should handle the fetch
-   */
-  static func add(data:Any, context:NSManagedObjectContext) -> Self?
-  
-  /**
-   This method is called by all managedObjects in order to add / update multiple
-   - parameter data:      The data received for update
-   - parameter context:   The context that should handle the fetch
-   */
-  static func addMultiple(data:Array<Any>, context:NSManagedObjectContext)-> Array<Self>
+    /**
+     This method is called by all managedObjects in order to add / update
+     - parameter data:      The data received for update
+     - parameter context:   The context that should handle the fetch
+     */
+    static func add(data:Any, context:NSManagedObjectContext) -> Self?
+    
+    /**
+     This method is called by all managedObjects in order to add / update multiple
+     - parameter data:      The data received for update
+     - parameter context:   The context that should handle the fetch
+     */
+    static func addMultiple(data:Array<Any>, context:NSManagedObjectContext)-> Array<Self>
 }
 
-// MARK: - AddManagedObject Implementation 
+// MARK: - AddManagedObject Implementation
 
 public extension AddManagedObject where Self: UpdateManangedObject, Self: NSManagedObject {
-  
-  @available(iOS 10.0, *)
-  static public func add(data:Any, context:NSManagedObjectContext) -> Self? {
-    let obj = Self.init(context: context)
-    obj.update(with: data)
-    return obj
-  }
-  
-  static public func addMultiple(data:Array<Any>, context:NSManagedObjectContext)-> Array<Self> {
-    var objects = [Self]()
-    for entry in data {
-      if let object = self.add(data: entry, context: context) {
-        objects.append(object)
-      }
+    
+    @available(iOS 10.0, *)
+    static public func add(data:Any, context:NSManagedObjectContext) -> Self? {
+        let obj = Self.init(context: context)
+        obj.update(with: data)
+        return obj
     }
-    return objects
-  }
+    
+    static public func addMultiple(data:Array<Any>, context:NSManagedObjectContext)-> Array<Self> {
+        var objects = [Self]()
+        for entry in data {
+            if let object = self.add(data: entry, context: context) {
+                objects.append(object)
+            }
+        }
+        return objects
+    }
 }
 
 // MARK: - SaveManagedObject
 
 public protocol SaveManagedObject: class {
-  /**
-   This method is called by all managedObjects in order to save single
-   - parameter data:          The data received for saving
-   - parameter context:       The context that should handle the fetch
-   - parameter completion:    The block that is called after the process completed
-   */
-  static func saveSingle(data:Any, context:NSManagedObjectContext, completion:@escaping CompletionBlock)
-  
-  /**
-   This method is called by all managedObjects in order to save multiple
-   - parameter data:          The data received for saving
-   - parameter context:       The context that should handle the fetch
-   - parameter completion:    The block that is called after the process completed
-   */
-  static func saveMultiple(data:Array<Any>, context:NSManagedObjectContext, completion:@escaping CompletionBlock)
+    /**
+     This method is called by all managedObjects in order to save single
+     - parameter data:          The data received for saving
+     - parameter context:       The context that should handle the fetch
+     - parameter completion:    The block that is called after the process completed
+     */
+    static func saveSingle(data:Any, context:NSManagedObjectContext, completion:@escaping CompletionBlock)
+    
+    /**
+     This method is called by all managedObjects in order to save multiple
+     - parameter data:          The data received for saving
+     - parameter context:       The context that should handle the fetch
+     - parameter completion:    The block that is called after the process completed
+     */
+    static func saveMultiple(data:Array<Any>, context:NSManagedObjectContext, completion:@escaping CompletionBlock)
 }
 
 // MARK: - SaveManagedObject Implementation
 
 public extension SaveManagedObject where Self: AddManagedObject {
-  
-  static public func saveSingle(data:Any, context:NSManagedObjectContext, completion:@escaping CompletionBlock) {
-    context.perform {
-      _ = self.add(data: data, context: context)
-      context.save(completion: completion)
+    
+    static public func saveSingle(data:Any, context:NSManagedObjectContext, completion:@escaping CompletionBlock) {
+        context.perform {
+            _ = self.add(data: data, context: context)
+            context.save(completion: completion)
+        }
     }
-  }
-  
- static public func saveMultiple(data:Array<Any>, context:NSManagedObjectContext, completion:@escaping CompletionBlock) {
-    context.perform {
-      _ = self.addMultiple(data: data, context: context)
-      // Save the context
-      context.save(completion: completion)
+    
+    static public func saveMultiple(data:Array<Any>, context:NSManagedObjectContext, completion:@escaping CompletionBlock) {
+        context.perform {
+            _ = self.addMultiple(data: data, context: context)
+            // Save the context
+            context.save(completion: completion)
+        }
     }
-  }
 }
 
 // MARK: - Fetch
 
 public protocol Fetch {
-  /**
-   Use this method i order to fetch some context based on some predicate 
-   - parameter predicate: The predicte that is used for fetch 
-   - parameter context:   The context that will handle the fetch 
-   - return: An instance of NSArray containing Self or nil 
-   */
-  static func fetch(with predicate: NSPredicate?, in context: NSManagedObjectContext) throws -> Array<Self>?
+    /**
+     Use this method i order to fetch some context based on some predicate
+     - parameter predicate: The predicte that is used for fetch
+     - parameter context:   The context that will handle the fetch
+     - return: An instance of NSArray containing Self or nil
+     */
+    static func fetch(with predicate: NSPredicate?, in context: NSManagedObjectContext) throws -> Array<Self>?
 }
 
 // MARK: - Fetch Implementation
 
 public extension Fetch where Self:NSManagedObject {
-  
-  @available(iOS 10.0, *)
-  public static func fetch(with predicate: NSPredicate?, in context: NSManagedObjectContext) throws -> Array<Self>? {
-    guard let request = Self.fetchRequest() as? NSFetchRequest<Self> else {
-      return nil
+    
+    @available(iOS 10.0, *)
+    public static func fetch(with predicate: NSPredicate?, in context: NSManagedObjectContext) throws -> Array<Self>? {
+        guard let request = Self.fetchRequest() as? NSFetchRequest<Self> else {
+            return nil
+        }
+        request.predicate = predicate
+        return try context.fetch(request)
     }
-    request.predicate = predicate
-    return try context.fetch(request)
-  }
 }
